fb4faa79c23790bdc3b21166b70b32699dfb9ada
GMartin03 <martingomez2003174@hotmail.com>
2023-01-19 20:06:47 -0300

    footer creado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./layout/header/Header"; 
+import Footer from "./layout/footer/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from "./layout/home/Home";
@@ -12,15 +13,16 @@ function App() {
   return (
     <>
       <BrowserRouter>            
-      <Header/>
-      <Routes>        
-      <Route path='/' element={<Home />}/>
-      <Route path='/ProductPage' element={<ProductPage />}/>
-        <Route path='/RecoverPassword' element={<RecoverPassword />}/>
-        <Route path='/EditPassword' element={< EditPassUser/>}/>       
-        <Route path='/Admin' element={<Administrator />}/>
-      </Routes>
-    </BrowserRouter>    
+        <Header/>
+        <Routes>        
+        <Route path='/' element={<Home />}/>
+        <Route path='/ProductPage' element={<ProductPage />}/>
+          <Route path='/RecoverPassword' element={<RecoverPassword />}/>
+          <Route path='/EditPassword' element={< EditPassUser/>}/>       
+          <Route path='/Admin' element={<Administrator />}/>
+        </Routes>
+      </BrowserRouter>    
+      <Footer/>
     </>
   );
 }
--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import "./footer.css"
+import { 
+  FaInstagramSquare,
+  FaTwitterSquare, 
+  FaFacebookSquare
+} from "react-icons/fa"
+import Logo from "../../components/assets/logo.png"
+import qrCode from "../../components/assets/qrcode.png"
 
 const Footer = () => {
   return (
-    <div className='footer_container'>
+    <div className='footerContainer'>
+        <div className='footerLg'>
+          <div>
+            <div>
+              <img src={Logo}/>
+            </div>
+            <div>
+              <div>Nuestras redes sociales</div>
+              <div><FaInstagramSquare size={25}/><a href='https://www.instagram.com/'>Instagram</a></div>
+              <div><FaTwitterSquare size={25}/><a href='https://twitter.com/'>Twitter</a></div>
+              <div><FaFacebookSquare size={25}/><a href='https://www.facebook.com/'>Facebook</a></div>
+            </div>
+            <div>
+              <div>Politicas</div>
+              <div><a href=''>Terminos y condiciones</a></div>
+              <div><a href=''>Politicas de privacidad</a></div>
+              <div><a href=''>Politicas de envio</a></div>
+            </div>
+            <div>
 
+              <img src={qrCode}/>
+            </div>
+          </div>    
+          <div>
+            <p>&copy; Derechos Reservados</p>
+          </div>    
+        </div>
+
+        <div className='footerMobile'>
+            <div>             
+              <FaInstagramSquare size={35}/>
+              <FaTwitterSquare size={35}/>
+              <FaFacebookSquare size={35}/>
+            </div>
+            <div>
+              <img src={Logo} alt="Tecno"/>
+            </div>
+        </div>
     </div>
   )
 }
