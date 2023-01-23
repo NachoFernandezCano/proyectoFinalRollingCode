051c08a54fd31459d65062a409ce93ced0f86939
AlexiaTonin <atoninelias@gmail.com>
2023-01-23 15:47:19 -0300

    botonera de categorias y agregue carpeta de imagenes de productos

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,29 +1,41 @@
-import ListGroup from 'react-bootstrap/ListGroup';
-import { Button } from "react-bootstrap";
+import { Button } from 'react-bootstrap';
+import {FaHeadset} from 'react-icons/fa'
+import {FiMonitor, FiWatch, FiMoreHorizontal} from 'react-icons/fi'
+import {GiSmartphone} from 'react-icons/gi'
+import {BsLaptop} from 'react-icons/bs'
+import "./categories.css";
 
 function Categories() {
   return (
     <>
-      <ListGroup horizontal>
-        <ListGroup.Item>
-          <Button>Notebooks</Button>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Button>Celulares</Button>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Button>Monitores</Button>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Button>Auriculares</Button>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Button>Smartwatch</Button>
-        </ListGroup.Item>
-        <ListGroup.Item>
-          <Button>Otros</Button>
-        </ListGroup.Item>
-      </ListGroup>
+      <div className='d-flex justify-content-center'>
+        <div className='d-flex p-2 gap-1'>
+          <Button className="btnRP" active>
+            <BsLaptop className='cIcon'/>
+            Notebooks
+          </Button>
+          <Button className="btnRP" active>
+            <GiSmartphone className='cIcon'/>
+            Celulares
+          </Button>
+          <Button className="btnRP" active>
+            <FiMonitor className='cIcon'/>
+            Monitores
+          </Button>
+          <Button className="btnRP" active>
+            <FaHeadset className='cIcon'/>
+            Auriculares
+          </Button>
+          <Button className="btnRP" active>
+            <FiWatch className='cIcon'/>
+            Smartwatch
+          </Button>
+          <Button className="btnRP" active>
+            <FiMoreHorizontal className='cIcon'/>
+            Otros
+          </Button>
+        </div>
+      </div>
     </>
   );
 }
