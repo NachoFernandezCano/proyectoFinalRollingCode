d2b9efdd8f72730e9fef0290b3713157102ea254
Nacho Fernandez Cano <you@example.com>
2023-01-28 18:54:11 -0300

    pull de develop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Homepage from './pages/homePage/homePage';
+import Homepage from './Pages/homePage/homePage';
 import RecoverPassword from './components/Form/account/RecoverPassword';
 import EditPassUser from './components/Form/account/editpassword/EditPassUser';
 import Administrator from './components/Admin/Administrator';
-import ProductPage from './components/ProductPage/ProductPage';
+import ProductPage from './components/productPage/ProductPage';
 import Table from './components/Table/Table'
 
 function App() {
