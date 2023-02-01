09ef8867fb0855f1adedf0d59039f2c7072bdbed
AlexiaTonin <atoninelias@gmail.com>
2023-01-31 21:37:20 -0300

    destacados creados y funcionando, pagina error 404 y organizacion de carpetas

---
+++ src/Pages/homePage/homePage.js
@@ -0,0 +1,16 @@
+import Slider from "../../components/Slider/Slider";
+import Home from "../../components/Home/Home";
+import Categories from "../../components/Categories/Categories"
+
+
+const Homepage = () => {
+  return ( 
+    <>
+    <Slider/>
+    <Home/>
+    <Categories/>
+    </>
+  );
+}
+
+export default Homepage;
