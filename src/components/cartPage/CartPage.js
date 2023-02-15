0859bdacc8b522466e1df6c7a84548333a8fd63a
GMartin03 <martingomez2003174@hotmail.com>
2023-02-15 11:13:23 -0300

    esquema de la cartPage

--- src/components/cartPage/CartPage.js
+++ src/components/cartPage/CartPage.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
-import { Table } from 'react-bootstrap'
+import "./cartPage.css"
 
 const CartPage = () => {
 
@@ -13,32 +13,116 @@ const CartPage = () => {
       console.log(info)
     }
 
-  return (
-    <>
-      <div>
-        <h2>Carrito de compra</h2>
-      </div>
-      <div>
-        <Table>
-          <thead>
-            <th>Producto</th>
-            <th>Nombre</th>
-            <th>Descripción</th>
-            <th>Precio</th>
-            <th>Cantidad</th>
-          </thead>
-          <tbody>
-            <tr>
-              <td>a</td>
-              <td>a</td>
-              <td>a</td>
-              <td>a</td>
-              <td>a</td>
-            </tr>
-          </tbody>
-        </Table>
-      </div>
-    </>
+return (
+  <>
+      <section class="mt-5">
+        <div class="container">
+            <div class="cart">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col"class="text-white">Product</th>
+                            <th scope="col"class="text-white">Price</th>
+                            <th scope="col"class="text-white">Quantity</th>
+                            <th scope="col"class="text-white">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div class="main">
+                                    <div class="d-flex">
+                                    <img src=""alt=""/>
+                                    </div>
+                                    <div class="des">
+                                        <p>lorem ipsum</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <h6>$20.00</h6>
+                            </td>
+                            <td>
+                                <div class="counter">
+                                    <i class="fas fa-angle-down"></i>
+                                    <input class="input-number"type="text" value="1"min="0"max="10"/>                                    
+                                    <i class="fas fa-angle-up"></i>
+                                </div>
+                            </td>
+                            <td>
+                                <h6>$20.00</h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="main">
+                                    <div class="d-flex">
+                                    <img src="" alt=""/>
+                                    </div>
+                                    <div class="des">
+                                        <p>lorem ipsum</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <h6>$20.00</h6>
+                            </td>
+                            <td>
+                                <div class="counter">
+                                    <i class="fas fa-angle-down"></i>
+                                    <input class="input-number"type="text" value="1"min="0"max="10"/>
+                                    <i class="fas fa-angle-up"></i>
+                                </div>
+                            </td>
+                            <td>
+                                <h6>$20.00</h6>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="main">
+                                    <div class="d-flex">
+                                      <img src="images/cart-1.jpg"alt=""/>
+                                    </div>
+                                    <div class="des">
+                                        <p>lorem ipsum</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <h6>$20.00</h6>
+                            </td>
+                            <td>
+                                <div class="counter">
+                                    <i class="fas fa-angle-down"></i>
+                                    <input class="input-number"type="text" value="1"min="0"max="10"/>
+                                    <i class="fas fa-angle-up"></i>
+                                </div>
+                            </td>
+                            <td>
+                                <h6>$20.00</h6>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
+    </section>
+    <div class="col-lg-4 offset-lg-4">
+        <div class="checkout">
+            <ul>
+                <li class="subtotal">subtotal
+                    <span>$60.00</span>
+                </li>
+                <li class="cart-total">Total
+                <span>$60.00</span></li>
+            </ul>
+            <a href="#"class="proceed-btn">Proceed to Checkout</a>
+        </div>
+    </div>
+  </>
   )
 }
 
