f256969ef312a046bd2565c5b96515083464300b
NachoFernandezCano <101266844+NachoFernandezCano@users.noreply.github.com>
2023-03-08 17:27:33 -0300

    Corregido register, ya funcional con todos los datos

--- src/components/productPage/ProductPage.js
+++ src/components/productPage/ProductPage.js
@@ -17,6 +17,7 @@ import Visa from "../../assets/images/mediosDePago/visa.svg";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
+import { useCartContext } from "../../context/cartContext";
 
 const ProductPage = () => {
   const { id } = useParams();
@@ -60,7 +61,6 @@ useEffect(() => {
       });
     }
   };
-
 
   return (
     <div className="productPageContainer">
@@ -133,7 +133,7 @@ useEffect(() => {
             <Button className="btn-comprarYa">Comprar</Button>
           </div>
           <div className="containerBtnAgregarAlcarrito">
-            <Button className="btn-agregarAlCarrito">Agregar al carrito</Button>
+            <Button className="btn-agregarAlCarrito" /* onClick={handleAddToCart}  */>Agregar al carrito</Button>
           </div>
         </div>
         <div className="containerMasCaracteristicas">
